0c2816b4be54fa0c4f0d692dac739eda91fc3ac7
Mohammad Moid Siddiqui <moid@192.168.1.2>
2022-08-25 12:29:44 +0530

    init algo

---
+++ insertionSort.js
@@ -0,0 +1,9 @@
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++){
+       for(let j = i - 1; j >= 0; j--){
+        
+       }
+    }
+
+    return arr;
+}
---
+++ selectionSort.js
@@ -0,0 +1,15 @@
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let lowest = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[lowest] > arr[j]){
+                lowest = j;
+            }
+        }
+        if (i !== lowest)
+            [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+    }
+    return arr;
+}
+
+console.log(selectionSort([2, 22, 33,9, 5, 6]))
